829f2a54532c16b70619c1a6981b5a82e2482638
Manvir <sehmbimanvir@gmail.com>
2020-05-07 13:36:59 +0530

    Unique Validation Added

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -2,6 +2,22 @@ import jwt from 'jsonwebtoken'
 import Settings from '../config';
 import Validator from 'validatorjs'
 import ValidationException from '../exceptions/ValidationException'
+import mongoose from 'mongoose'
+
+Validator.registerAsync('unique', function (value, attributes, req, passes) {
+  const parts = attributes.split(',')
+  if (!mongoose.models[parts[0]]) {
+    throw new Error(`${parts[0]} model not found.`)
+  }
+  const Schema = mongoose.model(parts[0])
+  const column = parts[1]
+  Schema.count({ [column]: value }, (err, count) => {
+    if (count) {
+      return passes(false, 'This email address has already been taken.');
+    }
+    passes()
+  })
+})
 
 export const validateToken = (req, res, next) => {
   const token = req.headers['x-access-token']
@@ -20,14 +36,15 @@ export const validateToken = (req, res, next) => {
 
 export const validation = rules => {
   return (req, res, next) => {
+    let validation = new Validator(req.body, rules)
     try {
-      let validation = new Validator(req.body, rules)
-      if (validation.fails()) {
-        throw new ValidationException(validation.errors)
-      }
-      next()
-    } catch (error) {
-      next(error)
+      validation.checkAsync(() => {
+        next()
+      }, () => {
+        next(new ValidationException(validation.errors))
+      })
+    } catch (err) {
+      next(err)
     }
   }
 }
